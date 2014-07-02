da9d173d35a73df1f0209b5746f720d613d67f39
poseidoneros <apfishing.jones@gmail.com>
2014-07-02 16:35:43 +0100

    3. mod class and start of file structure

---
+++ src/main/java/com/poseidoneros/genecraft/GeneCraft.java
@@ -0,0 +1,30 @@
+package com.poseidoneros.genecraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="Genecraft", name="genecraft", version="1.7.2-0.1")
+
+public class GeneCraft
+{
+@Mod.Instance("Genecraft")
+    public static GeneCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
