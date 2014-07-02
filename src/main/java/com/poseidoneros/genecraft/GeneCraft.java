5566a402b53cfe92198b8dc703cd61aafc5c77f5
poseidoneros <apfishing.jones@gmail.com>
2014-07-02 17:15:21 +0100

    4.mod file + mcmod.info

--- src/main/java/com/poseidoneros/genecraft/GeneCraft.java
+++ src/main/java/com/poseidoneros/genecraft/GeneCraft.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="Genecraft", name="genecraft", version="1.7.2-0.1")
+@Mod(modid="Genecraft", name="Gene Craft", version="1.7.2-0.1")
 
 public class GeneCraft
 {
